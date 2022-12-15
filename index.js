c53202b6b751e2f0ff9de4ea2622c7cf141c4a71
Aldi Maulana <simaul.noir@gmail.com>
2022-12-15 10:34:26 +0700

    added index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const submitForm = document.getElementById('form');
+  submitForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    addTodo();
+  })
+})
 
+function addTodo() {
+  const textTodo = document.getElementById('title').value;
+  const timestamp = document.getElementById('date').value;
+
+  const generateID = generateId();
+  const todoObject = generateTodoObject(generateID, textTodo, timestamp);
+  todos.push(todoObject);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
